297251b84103224379585758faca3304fa4af38a
morarodgers <morarodgers@gmail.com>
2023-10-31 20:34:21 +0300

    Correction changes for week 4

--- 02-express-tutorial/controllers/people.js
+++ 02-express-tutorial/controllers/people.js
@@ -18,13 +18,18 @@ const getPersonById = (req, res) => {
 };
 
 const addPerson = (req, res) => {
-  const nextId = people.length > 0 ? people[people.length - 1].id + 1 : 1;
-  people.push({ id: nextId, name: req.body.name });
+//  const nextId = people.length > 0 ? people[people.length - 1].id + 1 : 1;
+//  people.push({ id: nextId, name: req.body.name });
+const {name} = req.body
+if (name) {
   const newName = req.body.name;
   const newPerson = { id: nextId, name: newName };
   people.push(newPerson);
 
   res.status(201).json(newPerson);
+} else {
+  res.status(400).json({success: false, message: 'Please provide a name'});
+}
 };
 
 const updatePeople = (req, res) => {
@@ -51,7 +56,8 @@ const deletePerson = (req, res) => {
         return res.status(404).json({success: false, message: `No person with id ${req.params.id}` })
     }
     //const newPeople = people.filter((person) => person.id !== Number(req.params.id))
-    const index = people.findIndex(person => person.id === Number(req.params.id)); people.splice(index, 1);
+    const index = people.findIndex(person => person.id === Number(req.params.id));
+    people.splice(index, 1);
     return res.status(200).json({success: true, data: index})
 }
 
