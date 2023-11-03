9ab7debcec104d3154714fa93af44ef8d346cd18
morarodgers <morarodgers@gmail.com>
2023-11-03 22:59:18 +0300

    Changed 404 response as guided for week 5

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,51 +1,51 @@
 const Task = require('../models/Task')
 
 const getAllTasks = async (req, res) => {
-    const tasks = await Task.find({})
-    res.status(200).json({tasks})
-  }
+  const tasks = await Task.find({})
+  res.status(200).json({tasks})
+}
 
 const createTask = async (req, res) => {
-    const task = await Task.create(req.body)
-    //res.send('Create task')
-    res.status(201).json({task})
+  const task = await Task.create(req.body)
+  //res.send('Create task')
+  res.status(201).json({task})
 }
 
 const getTask = async (req, res, next) => {
-    const { id: taskID } = req.params
-    const task = await Task.findOne({ _id: taskID })
-    if (!task) {
-      return next(createCustomError(`The task with id : ${taskID}, does not exist`, 404))
-    }
-    res.status(200).json({task})
+  const { id: taskID } = req.params
+  const task = await Task.findOne({ _id: taskID })
+  if (!task) {
+    return res.status(404).json({success: false, message:`The task with id : ${taskID}, does not exist!`})
   }
+  res.status(200).json({task})
+}
 
 const updateTask = async (req, res, next) => {
-    const {id:taskID} = req.params
-    const task = await Task.findOneAndUpdate({_id:taskID}, req.body, {
-      new: true,
-      runValidators: true,
-    })
-    if (!task) {
-      return next(createCustomError(`The task with id : ${taskID}, does not exist`, 404))
-    }
-    res.status(200).json({task})
+  const {id:taskID} = req.params
+  const task = await Task.findOneAndUpdate({_id:taskID}, req.body, {
+    new: true,
+    runValidators: true,
+  })
+  if (!task) {
+    return res.status(404).json({success: false, message:`The task with id : ${taskID}, does not exist!`})
   }
+  res.status(200).json({task})
+}
 
 const deleteTask = async (req, res, next) => {
-    const {id:taskID} = req.params
-    const task = await Task.findOneAndDelete({_id:taskID})
-    if (!task) {
-      return next(createCustomError(`The task with id : ${taskID}, does not exist`, 404))
-    }
-    res.status(200).json({task})
+  const {id:taskID} = req.params
+  const task = await Task.findOneAndDelete({_id:taskID})
+  if (!task) {
+    return res.status(404).json({success: false, message:`The task with id : ${taskID}, does not exist!`})
   }
+  res.status(200).json({task})
+}
 
 
 module.exports = {
-    getAllTasks,
-    createTask,
-    getTask,
-    updateTask,
-    deleteTask
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask
 }
