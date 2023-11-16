20c551c675cb771054cafc8c472be9353bcb640e
morarodgers <morarodgers@gmail.com>
2023-11-16 09:15:32 +0300

    Updated corrections for week 7

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -57,10 +57,10 @@ const getAllProducts = async(req,res) => {
     } 
     if (fields) {
         const fieldList = fields.split(',').join(' ');
-        result = result.sort(fieldList)
+        result = result.select(fieldList)
     }
     const page =Number(req.query.page) || 1
-    const limit = Number(req.query) || 10
+    const limit = Number(req.query.limit) || 10
     const skip = (page - 1) * limit;
 
     result = result.skip(skip).limit(limit)
